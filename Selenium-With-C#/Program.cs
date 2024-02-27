7a167cde72ea434408f86ae55337601e7d17f732
Uzair Shahid <uzairshahid7111@gmail.com>
2024-02-27 13:14:05 +0000

    added custom method for get and set values

---
+++ Selenium-With-C#/Program.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_With_CSharp
+{
+   
+     class Program
+    {
+        // Initialize the driver
+        IWebDriver driver = new ChromeDriver();
+        //static void Main(string[] args)
+        //{
+           
+        //}
+
+
+
+        [SetUp]
+        public void Initialize()
+        {
+           // driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+        }
+
+        [Test] 
+        public void ExecuteTest()
+        {
+            IWebElement acceptCookies = driver.FindElement(By.ClassName("sy4vM"));
+            acceptCookies.Click();
+
+            IWebElement searchBoxElement = driver.FindElement(By.ClassName("gLFyf"));
+
+            // Perform Operation
+            searchBoxElement.SendKeys("Uzair Shahid");
+        }
+
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+        }
+    }
+}
